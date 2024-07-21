f19058ebdd65600429607d3101682b8eb4528033
Martin Saviev <114023121+MartinSaviev@users.noreply.github.com>
2024-07-21 10:23:25 +0300

    Update CreateNewVideo component add post requests

--- client/components/video/create-new-video/CreateNewVideo.jsx
+++ client/components/video/create-new-video/CreateNewVideo.jsx
@@ -1,23 +1,92 @@
+import { useState } from "react";
+
 import styles from "./CreateNewVideo.module.css";
 import backgroundVideo from "./backgroundVideo.mp4";
+import {useNavigate } from "react-router-dom";
+
+const url = "http://localhost:3030/jsonstore/videos";
 
 export default function CreateNewVideo() {
+  let navigate = useNavigate();
+  const [values, setValues] = useState({
+    header: "",
+    videoUrl: "",
+  });
+
+  function changeNameHandler(ev) {
+    setValues((prevValues) => ({
+      ...prevValues,
+      header: ev.target.value,
+    }));
+  }
+
+  function changeUrlHandler(ev) {
+    setValues((prevValues) => ({
+      ...prevValues,
+      videoUrl: ev.target.value,
+    }));
+  }
+
+  const requestOptions = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(values),
+  };
+
+  console.log(values);
+  async function postHandler(event) {
+    event.preventDefault();
+
+    const response = await fetch(url, requestOptions);
+    const data = await response.json();
+    setValues({ header: "", videoUrl: "" });
+    console.log(data);
+    navigate("../Video");
+  }
+  console.log(values);
   return (
     <div className={styles.body}>
-      <video className={styles.video} src={backgroundVideo} type="video/mp4" autoPlay loop muted></video>
+      <video
+        className={styles.video}
+        src={backgroundVideo}
+        type="video/mp4"
+        autoPlay
+        loop
+        muted
+      ></video>
       <div className={styles.wrapper}>
         <div className={styles.title}>Create New Video</div>
-        <form className={styles.form}>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault;
+          }}
+          className={styles.form}
+        >
           <div className={styles.field}>
-            <input type="text" name="name" required />
+            <input
+              type="text"
+              name="name"
+              value={values.header}
+              onChange={changeNameHandler}
+            />
             <label>Име</label>
           </div>
           <div className={styles.field}>
-            <input type="text" name="img" required />
+            <input
+              type="text"
+              name="img"
+              value={values.videoUrl}
+              onChange={changeUrlHandler}
+            />
             <label>Изображение (URL)</label>
           </div>
-          <div className={styles['submit-btn-container']}>
-            <input type="submit" className={styles['submit-btn']} value="Submit" />
+          <div className={styles["submit-btn-container"]}>
+            <input
+              type="submit"
+              className={styles["submit-btn"]}
+              value="Submit"
+              onClick={postHandler}
+            />
           </div>
         </form>
       </div>
