902c7ef4d6318aa89f3d25395252333ae2750541
Martin Saviev <114023121+MartinSaviev@users.noreply.github.com>
2024-07-28 22:19:46 +0300

    request to delete recipe

--- client/components/recipes/delete-recipe/DeleteRecipe.jsx
+++ client/components/recipes/delete-recipe/DeleteRecipe.jsx
@@ -2,15 +2,17 @@ import style from "./DeleteRecipe.module.css";
 import { Link, useNavigate, useParams } from "react-router-dom";
 
 import * as requester from "../../../src/api/requester";
-const ulr = 'recipes/recipes/';
+
+import {urls} from "../../../public/allUrls/urls.js"
 
 export default function DeleteRecipe() {
   let { userId } = useParams();
   let navigate = useNavigate();
 
   async function deleteRecipeHandler() {
-    const response = await requester.del(`${ulr}/${userId}`);
-    
+    const response = await requester.del(`${urls.recipes}/${userId}`);
+    console.log(response);
+    console.log(userId);
     if (response) {
         navigate('/recipes')
     }
